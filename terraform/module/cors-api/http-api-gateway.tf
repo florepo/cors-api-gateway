75cc5619fc8eca47133ec6fe8df38b0b45d8d959
flow1981 <florian.stagliano@gmx.de>
2021-02-21 22:04:01 +0000

    Set required permissions

--- terraform/module/cors-api/http-api-gateway.tf
+++ terraform/module/cors-api/http-api-gateway.tf
@@ -1,16 +1,61 @@
 # HTTP Api Gateway
-# requires terraform aws programmatic user needs APIGatewayAdministrator permission
 resource "aws_apigatewayv2_api" "api" {
   name          = "cors-proxy-api"
   protocol_type = "HTTP"
+
+  depends_on = [
+    aws_lambda_function.lambda
+  ]
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id               = aws_apigatewayv2_api.api.id
+  integration_type     = "AWS_PROXY"
+
+  integration_method   = "POST"
+  integration_uri      = aws_lambda_function.lambda.invoke_arn
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
+
+resource "aws_apigatewayv2_route" "api" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "api" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_deployment" "api" {
+  api_id      = aws_apigatewayv2_api.api.id
+  description = "API deployment"
+
+  triggers = {
+    redeployment = sha1(join(",", list(
+      jsonencode(aws_apigatewayv2_integration.lambda),
+      jsonencode(aws_apigatewayv2_route.api),
+    )))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Permission
-resource "aws_lambda_permission" "apigw" {
-	action        = "lambda:InvokeFunction"
-	function_name = aws_lambda_function.lambda.arn
-	principal     = "apigateway.amazonaws.com"
+data "aws_caller_identity" "current" { }
 
-	source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "allow_apigw_invoke"
+	function_name = aws_lambda_function.lambda.function_name
+  action        = "lambda:InvokeFunction"
+	principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_stage.api.execution_arn}/${aws_apigatewayv2_route.api.route_key}"
 }
 
+output "invoke_url" {
+  value = aws_apigatewayv2_stage.api.invoke_url
+}
--- terraform/module/cors-api/lambda.tf
+++ terraform/module/cors-api/lambda.tf
@@ -1,42 +1,51 @@
 # Data source containing the lambda function and for putting its zipped version
 data "archive_file" "lambda_zip" {
-  source_file = "../module/cors-api/lambda.js"
+  source_file = "../module/cors-api/index.js"
   type = "zip"
-  output_path = "../module/cors-api/lambda.zip"
+  output_path = "../module/cors-api/index.zip"
 }
 
 # Lambda Function
 resource "aws_lambda_function" "lambda" {
-  filename = "../module/cors-api/lambda.zip"
+  filename = "../module/cors-api/index.zip"
   function_name = "cors-proxy-js"
-  role = aws_iam_role.lambda.arn
+  role = aws_iam_role.lambda_execution_role.arn
   handler = "index.handler"
-
+  publish = true
   source_code_hash = "${filebase64sha256(data.archive_file.lambda_zip.output_path)}"
 
   runtime = "nodejs12.x"
 }
 
 # IAM role for Lambda Function
-resource "aws_iam_role" "lambda" {
-  name = "iam-lambda-role"
-   assume_role_policy = <<EOF
+resource "aws_iam_role" "lambda_execution_role" {
+  description        = "Execution role for Lambda functions"
+  name               = "iam-lambda-role"
+
+  assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": [
-          "lambda.amazonaws.com",
-          "apigateway.amazonaws.com"
+        "Version"  : "2012-10-17",
+        "Statement": [
+            {
+                "Action"   : "sts:AssumeRole",
+                "Principal": {  
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid"   : ""
+            }
         ]
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
 }
 EOF
+}
 
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_gateway_invoke_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
 }
